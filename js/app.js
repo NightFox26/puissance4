b40502e5a655e86e1a93c66600bd66aafa569723
Thierry <thierry.odier26@gmail.com>
2021-11-27 10:02:50 +0100

    creation du template V1

--- js/app.js
+++ js/app.js
@@ -5,12 +5,9 @@ let winChain = 4;
 new Grid(rows,columns);
 let joueur1 = new Player(1,"Joueur1", "pink");
 let joueur2 = new Player(2,"Joueur2", "dodgerblue");
-
-let tour = document.querySelector("#tour_joueur span")
 let sectionGrid = document.querySelector("#grid")
 
 joueur1.isPlaying = true;
-tour.textContent=joueur1.name
 
 // action sur le btn reset
 document.querySelector("#reset").addEventListener("click", function() {
@@ -22,9 +19,9 @@ function changePlayer(){
     joueur2.isPlaying = !joueur2.isPlaying;
    
     if(joueur1.isPlaying) {
-        tour.textContent=joueur1.name
+        
     } else {
-        tour.textContent=joueur2.name
+
     }
 }
 
